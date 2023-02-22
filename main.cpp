e78e614a5e377cc7e50482967e68b13e39933145
Jose Moreira <joserafita23@gmail.com>
2023-02-22 14:55:33 -0600

    fix letor

--- main.cpp
+++ main.cpp
@@ -3,21 +3,48 @@
 #include <stdio.h>
 #include <vector>
 #include <string.h>
+#include <dirent.h>
+#include "mkdisk.cpp"
 
 using namespace std;
 
-string *ObtenerParametro(char parametro[]){
-    char delimitador[] = "=";
-    string *vec;
-    char * token = strtok(parametro,delimitador);
-    if(token != NULL){
-        vec[0]=token;
+void LeerComando(char []);
+
+void execute(string ruta){
+    ifstream ejecutable;
+    ejecutable.open(ruta,ios::in);
+    string line;
+    if(ejecutable.fail()){
+        exit(1);
     }
-    token = strtok(NULL, delimitador);
-    if(token != NULL){
-        vec[1]=token;
+    while (getline(ejecutable, line)){
+        cout<<line<<endl;
+        LeerComando(&line[0]);
     }
-    return vec;
+}
+
+string ObtenerNombre(string parametro){
+    string vec;
+    string nombre = "";
+    for(int i = 0; i < parametro.length(); i++){
+        if(parametro.at(i) == '=') break;
+        char caracter = parametro.at(i);
+        nombre = nombre + caracter;
+    }
+    return nombre;
+}
+
+string ObtenerValor(string parametro){
+    string valor = "";
+    bool concatenar = false;
+    for(int i = 0; i < parametro.length(); i++){
+        if(concatenar){
+            char caracter = parametro.at(i);
+            valor = valor + caracter;
+        }
+        if(parametro.at(i) == '=') {concatenar = true;}
+    }
+    return valor;
 }
 
 void LeerComando(char comando[]){
@@ -27,14 +54,40 @@ void LeerComando(char comando[]){
     if(token != NULL){
             vec=token;
     }
-    cout<<vec<<endl;
+    //cout<<vec<<endl;
     if (vec=="execute"){
         token = strtok(NULL, delimitador);
-        vec = token;
-        string *compo = ObtenerParametro(&vec[0]);
+        string aux = token;
+        string compo[2];
+        compo[1]=ObtenerValor(aux);
+        compo[0]=ObtenerNombre(aux);
         if(compo[0]==">path"){
-            cout<<compo[1]<<endl;
+            execute(compo[1]);
         }
+    }
+    else if (vec=="mkdisk"){
+        mkdisk *actual=new mkdisk();
+        token = strtok(NULL, delimitador);
+        while (token != NULL){
+
+            string aux = token;
+            string compo[2];
+            compo[1]=ObtenerValor(aux);
+            compo[0]=ObtenerNombre(aux);
+            if(compo[0]==">path"){
+                actual->ruta = compo[1];
+            }else  if(compo[0]==">size"){
+                actual->tamanio = stoi(compo[1]);
+            }else  if(compo[0]==">unit"){
+                actual->unidad = compo[1][0];
+            }else  if(compo[0]==">fit"){
+                actual->fit = compo[1];
+            }
+            token = strtok(NULL, delimitador);
+        }
+
+        //actual->mk();
+
     }
 }
 
---
+++ mkdisk.cpp
@@ -0,0 +1,78 @@
+#ifndef MKDISK_H
+#define MKDISK_H
+
+#include <iostream>
+using namespace std;
+
+
+class mkdisk
+{
+public:
+    mkdisk();
+    int tamanio;
+    string ruta;
+    string fit;
+
+    char unidad ;
+    void makedisk();
+    void mk();
+
+};
+
+mkdisk::mkdisk(){
+    this->unidad='M';
+    this->fit="FF";
+}
+
+void mkdisk::mk(){
+    cout<<"entro"<<endl;
+    cout << "path: " << this->ruta << endl;
+    cout << "size: " << this->tamanio << endl;
+    cout << "unit: " << this->unidad << endl;
+    cout << "fit: " << this->fit << endl;
+
+}
+
+void mkdisk::makedisk(){
+    string path = this->ruta;
+    cout<<"Creando disco en: \n"<<path<<endl;
+    FILE *disco;
+    disco=fopen(path.c_str(),"wb");
+    if (disco==NULL){
+        exit(1);
+        cout<<"ERROR en creacion de disco \n"<<path;
+    }
+    fclose(disco);
+
+    disco=fopen(path.c_str(),"ab");
+    /*mbr actual;
+    actual.mbr_tamanio=tamanio;
+    time_t now;
+    now = time(NULL);
+
+    actual.mbr_fecha_creacion=now;
+    actual.mbr_dsk_asignature=rand()%1000;
+    fwrite(&actual,sizeof(actual),1,disco);*/
+    if (disco==NULL){
+    exit(1);
+    }
+
+    char Pesokilo [1024];
+    for(int j =0;j<1024;j++){
+        Pesokilo[j]=0;
+    }
+    int tam ;
+    if(tolower(this->unidad)=='k'){
+    tam = tamanio;
+    }
+    else if(tolower(this->unidad)=='m'){
+    tam = 1024* tamanio;
+    }
+
+    for(int i =0;i<tam;i++){
+        fwrite(&Pesokilo, sizeof(Pesokilo), 1, disco);
+    }
+    fclose(disco);
+}
+
+#endif
